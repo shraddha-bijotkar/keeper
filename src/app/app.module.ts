262b7ebb44f63b7c98ea9ea2d64af6f5f4f79428
Shraddha Bijotkar <shraddha.bijotkar@gmail.com>
2024-03-24 15:25:59 +0530

    Keeper | Added | Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NotesComponent,
+    SideBarComponent,
+    NavBarComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
